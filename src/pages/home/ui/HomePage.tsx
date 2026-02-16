4f1b372743200ea9c7eae1ec3b8b0134816973be
Proshka-dev <proshka_s@mail.ru>
2026-02-16 10:36:37 +0700

    Закончена настройка папок

---
+++ src/pages/home/ui/HomePage.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export function HomePage() {
+	return (
+		<div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black" >
+			Главная страница
+		</div>
+	);
+}
